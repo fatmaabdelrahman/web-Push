fe303bb99bf715faa7da802e71eb97b9f1e0d6f2
fatmaabdelrahman <fabdelrahman72@yahoo.com>
2022-05-08 01:21:21 +0200

    Refactore Push Functions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PushController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class,'index'])->name('home');
+
+//make a push notification.
+Route::get('/push',[PushController::class,'push'])->name('push');
+
+//store a push subscriber.
+Route::post('/push',[PushController::class,'store']);
